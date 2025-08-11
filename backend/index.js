15fceb5ddd9f70f70852366d66131dc19fc35aff
little_q_exist <3061982805@qq.com>
2025-08-11 20:04:59 +0800

    before exer 3.d

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ app.get('/api/notes/:id', (request, response, next) => {
 //     return String(maxId + 1)
 // }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
 
     const body = request.body
 
@@ -55,9 +55,11 @@ app.post('/api/notes', (request, response) => {
         // id: generateId()
     })
 
-    note.save().then(savedNote => {
-        response.json(savedNote)
-    })
+    note.save()
+        .then(savedNote => {
+            response.json(savedNote)
+        })
+        .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -100,6 +102,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(404).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
