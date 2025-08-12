51f15470ecd04835397cc8423c53d6b0f41153c9
little_q_exist <3061982805@qq.com>
2025-08-12 20:23:35 +0800

    exer 4.a step3

--- blog-list/index.js
+++ blog-list/index.js
@@ -1,37 +1,20 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const config = require('./utils/config')
+const Blog = require('./models/blogs')
+const blogRouter = require('./controllers/blogs')
 
 const app = express()
 
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number,
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb://localhost/bloglist'
+const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-    Blog.find({}).then((blogs) => {
-        response.json(blogs)
-    })
-})
+app.use('/api/blogs', blogRouter)
 
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog.save().then((result) => {
-        response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
+const PORT = config.PORT || 3003
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
