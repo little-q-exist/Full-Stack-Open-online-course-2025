d2e2aaaafb91df586c64014202285ff39c878983
little_q_exist <3061982805@qq.com>
2025-09-06 16:44:17 +0800

    exer 6.a -2 step 1

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -23,7 +23,25 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
-  return state
+  switch (action.type) {
+    case 'ADD_VOTE': {
+      const id = action.payload.id
+      const anecdoteToChange = state.find(anecdote => anecdote.id === id)
+      const newAnecdote = { ...anecdoteToChange, votes: anecdoteToChange.votes + 1 }
+
+      return state.map(anecdote => anecdote.id === id ? newAnecdote : anecdote)
+    }
+    default: return state
+  }
+}
+
+export const addVoteOf = (id) => {
+  return {
+    type: 'ADD_VOTE',
+    payload: {
+      id
+    }
+  }
 }
 
 export default reducer
