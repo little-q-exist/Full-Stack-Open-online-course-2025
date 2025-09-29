5e5627af0eebdfe56fc4821a94ace3c11eba80cd
little_q_exist <3061982805@qq.com>
2025-09-29 17:58:31 +0800

    Feature: add User component

---
+++ bloglist-frontend-redux/src/components/Users.jsx
@@ -0,0 +1,20 @@
+const Users = () => {
+    return (
+        <div>
+            <h2>Users</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>blogs created</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { }
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Users
