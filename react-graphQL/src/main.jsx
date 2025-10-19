c05c7acc854938f02fb58099b5eb987806926d32
little_q_exist <3061982805@qq.com>
2025-10-19 09:02:54 +0800

    8.b -1

---
+++ react-graphQL/src/main.jsx
@@ -0,0 +1,15 @@
+import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
+
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+)
