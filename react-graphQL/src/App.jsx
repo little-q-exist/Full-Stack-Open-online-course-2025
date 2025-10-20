e4f2fa1472074aa79324f8d59bb4c9e467b6c7e7
little_q_exist <3061982805@qq.com>
2025-10-20 20:01:44 +0800

    before exer 8.b

--- react-graphQL/src/App.jsx
+++ react-graphQL/src/App.jsx
@@ -1,26 +1,43 @@
-import { gql, useQuery } from '@apollo/client'
+import { useQuery } from '@apollo/client'
 import Persons from './component/Persons'
+import PersonForm from './component/PersonForm'
+import PhoneForm from './component/PhoneForm'
+import { ALL_PERSONS } from './queries'
+import { useState } from 'react'
 
-const ALL_PERSONS = gql`
-query {
-  allPersons {
-    name
-    phone
-    id
+const Notify = ({ errorMessage }) => {
+  if (!errorMessage) {
+    return null
   }
+  return (
+    <div style={{ color: 'red' }}>
+      {errorMessage}
+    </div>
+  )
 }
-`
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+
   const result = useQuery(ALL_PERSONS)
 
   if (result.loading) {
     return <div>loading...</div>
   }
 
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 5000)
+  }
+
   return (
     <div>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
+      <PersonForm setError={notify} />
+      <PhoneForm setError={notify}/>
     </div>
   )
 }
--- react-graphQL/src/component/Persons.jsx
+++ react-graphQL/src/component/Persons.jsx
@@ -1,19 +1,6 @@
-import { gql, useQuery } from '@apollo/client'
+import { useQuery } from '@apollo/client'
 import { useState } from 'react'
-
-const FIND_PERSON = gql`
- query findPersonByName($nameToSearch: String!) {
-    findPerson(name: $nameToSearch) {
-      name
-      phone
-      id
-      address {
-        street
-        city
-      }
-    }
-  }
-`
+import { FIND_PERSON } from '../queries'
 
 const Person = ({ person, onClose }) => {
     return (
