21b48bf5d3771ade0f0c85af8965e1cec810b0f0
little_q_exist <3061982805@qq.com>
2025-11-03 18:08:48 +0800

    exer 8.c -3

--- library-backend/index.js
+++ library-backend/index.js
@@ -4,6 +4,7 @@ const { v4: uuidv4 } = require('uuid')
 const { startStandaloneServer } = require('@apollo/server/standalone')
 const Book = require('./models/Book')
 const Author = require('./models/Author')
+const { GraphQLError } = require('graphql')
 
 require('dotenv').config()
 
@@ -19,102 +20,6 @@ mongoose.connect(MONGODB_URI)
         console.log('error connection to MongoDB:', error.message)
     })
 
-/*
-let authors = [
-    {
-        name: 'Robert Martin',
-        id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-        born: 1952,
-    },
-    {
-        name: 'Martin Fowler',
-        id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-        born: 1963
-    },
-    {
-        name: 'Fyodor Dostoevsky',
-        id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-        born: 1821
-    },
-    {
-        name: 'Joshua Kerievsky', // birthyear not known
-        id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-    },
-    {
-        name: 'Sandi Metz', // birthyear not known
-        id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-    },
-]
-
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- *
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conexión con el libro
-*/
-/*
-let books = [
-    {
-        title: 'Clean Code',
-        published: 2008,
-        author: 'Robert Martin',
-        id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-        genres: ['refactoring']
-    },
-    {
-        title: 'Agile software development',
-        published: 2002,
-        author: 'Robert Martin',
-        id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-        genres: ['agile', 'patterns', 'design']
-    },
-    {
-        title: 'Refactoring, edition 2',
-        published: 2018,
-        author: 'Martin Fowler',
-        id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-        genres: ['refactoring']
-    },
-    {
-        title: 'Refactoring to patterns',
-        published: 2008,
-        author: 'Joshua Kerievsky',
-        id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-        genres: ['refactoring', 'patterns']
-    },
-    {
-        title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-        published: 2012,
-        author: 'Sandi Metz',
-        id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-        genres: ['refactoring', 'design']
-    },
-    {
-        title: 'Crime and punishment',
-        published: 1866,
-        author: 'Fyodor Dostoevsky',
-        id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-        genres: ['classic', 'crime']
-    },
-    {
-        title: 'Demons',
-        published: 1872,
-        author: 'Fyodor Dostoevsky',
-        id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-        genres: ['classic', 'revolution']
-    },
-]
-
-/*
-  you can remove the placeholder query once your first one has been implemented 
-*/
 
 const typeDefs = `
     type Book {
@@ -135,7 +40,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author:String, genre: [String!]): [Book!]!
+    allBooks(genre: [String!]): [Book!]!
     allAuthors: [Author!]!
   }
 
@@ -155,8 +60,8 @@ const typeDefs = `
 
 const resolvers = {
     Author: {
-        bookCount: (root) => {
-            return Book.find({ author: root.name }).countDocuments()
+        bookCount: async (root) => {
+            return await Book.find({ author: root.name }).countDocuments()
         }
     },
 
@@ -179,34 +84,63 @@ const resolvers = {
                 filter.genres = { $in: args.genre }
             }
 
-            return Book.find(filter)
+            return Book.find(filter).populate('author')
         },
         allAuthors: () => Author.find({}),
     },
 
     Mutation: {
         addBook: async (root, args) => {
-            const authorExists = await Author.find({ name: args.author.name }).countDocuments() > 0
-            if (!authorExists) {
+            const authorName = args.author
+            const existedAuthor = await Author.findOne({ name: authorName })
+            if (!existedAuthor) {
                 const newAuthor = {
-                    name: args.author.name,
+                    name: authorName,
                     id: uuidv4(),
                 }
-                const author = new Author(newAuthor)
-                await author.save()
+                const author = await new Author(newAuthor).save()
+                const newBook = { ...args, author: author._id, id: uuidv4() }
+
+                try {
+                    return await new Book(newBook).save()
+                } catch (error) {
+                    throw new GraphQLError('Book title must be unique and at least 5 characters long.', {
+                        extensions: {
+                            code: 'BAD_USER_INPUT',
+                            invalidArgs: args.title,
+                            error,
+                        },
+                    })
+                }
             }
-            const newBook = { ...args, id: uuidv4() }
-            const book = new Book(newBook)
-            return await book.save()
+            const newBook = { ...args, author: existedAuthor._id, id: uuidv4() }
+
+            try {
+                return await new Book(newBook).save()
+            } catch (error) {
+                throw new GraphQLError('Book title must be unique and at least 5 characters long.', {
+                    extensions: {
+                        code: 'BAD_USER_INPUT',
+                        invalidArgs: args.title,
+                        error,
+                    },
+                })
+            }
         },
-        editAuthor: (root, args) => {
-            const author = authors.find(a => a.name === args.name)
-            if (!author) {
-                return null
+        editAuthor: async (root, args) => {
+            const author = await Author.findOne({ name: args.name })
+            author.born = args.setBornTo
+            try {
+                return await author.save()
+            } catch (error) {
+                throw new GraphQLError('Author name must be unique and at least 4 characters long.', {
+                    extensions: {
+                        code: 'BAD_USER_INPUT',
+                        invalidArgs: args.name,
+                        error,
+                    },
+                })
             }
-            const updatedAuthor = { ...author, born: args.setBornTo }
-            authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
-            return updatedAuthor
         }
     },
 }
