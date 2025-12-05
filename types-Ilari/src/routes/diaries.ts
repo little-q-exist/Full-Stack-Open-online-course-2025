9285be476db91dad4d18dcb8d2da9a90ef46c8e3
little_q_exist <3061982805@qq.com>
2025-12-05 18:11:44 +0800

    before exer 9.c -2

---
+++ types-Ilari/src/routes/diaries.ts
@@ -0,0 +1,17 @@
+import { Response, Router } from 'express';
+
+import { NonSensitiveDiaryEntry } from '../types';
+
+import diaryService from '../services/diaryService';
+
+const router = Router();
+
+router.get('/', (_req, res: Response<NonSensitiveDiaryEntry[]>) => {
+    res.send(diaryService.getNonSensitiveEntries());
+});
+
+router.post('/', (_req, res) => {
+    res.send('Saving a diary!');
+});
+
+export default router;
