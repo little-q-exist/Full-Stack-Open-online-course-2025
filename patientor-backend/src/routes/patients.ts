08ec09277968b8313844e778f1ee0318403f7f16
little_q_exist <3061982805@qq.com>
2025-12-10 10:46:44 +0800

    exer 9.c finished

--- patientor-backend/src/routes/patients.ts
+++ patientor-backend/src/routes/patients.ts
@@ -1,18 +1,39 @@
-import express from 'express';
+import { Router, NextFunction, Request, Response } from 'express';
+
+import { ZodError } from 'zod';
 
 import patientsServices from '../services/patients';
-import toNewPatient from '../utils';
+import { NewPatientSchema } from '../utils';
+import { NewPatient, Patient } from '../types';
 
-const router = express.Router();
+const router = Router();
 
 router.get('/', (_req, res) => {
     res.json(patientsServices.getPatients());
 });
 
-router.post('/', (req, res) => {
-    const newPatient = toNewPatient(req.body);
-    const addedPatient = patientsServices.addPatient(newPatient);
+const newPatientParser = (req: Request, _res: Response, next: NextFunction) => {
+    try {
+        NewPatientSchema.parse(req.body);
+        next();
+    } catch (error: unknown) {
+        next(error);
+    }
+};
+
+const errorHandler = (error: unknown, _req: Request, res: Response, _next: NextFunction) => {
+    if (error instanceof ZodError) {
+        res.status(400).json({ error: error.issues });
+    } else if (error instanceof Error) {
+        res.status(400).json({ error: 'unknown error' });
+    }
+};
+
+router.post('/', newPatientParser, (req: Request<unknown, unknown, NewPatient>, res: Response<Patient>) => {
+    const addedPatient = patientsServices.addPatient(req.body);
     res.json(addedPatient);
 });
+
+router.use(errorHandler);
 
 export default router;
--- patientor-backend/src/utils.ts
+++ patientor-backend/src/utils.ts
@@ -1,72 +1,13 @@
-import { Gender, NewPatient } from './types';
+import z from 'zod';
+import { Gender } from './types';
 
-const isString = (text: unknown): text is string => {
-    return typeof text === 'string' || text instanceof String;
-};
-
-const isDate = (date: string): boolean => {
-    return Boolean(Date.parse(date));
-};
-
-const isGender = (gender: string): gender is Gender => {
-    return Object.values(Gender).map(val => val.toString()).includes(gender);
-};
-
-const parseName = (name: unknown): string => {
-    if (!name || !isString(name)) {
-        throw new Error('Incorrect or missing name');
-    }
-    return name;
-};
-
-const parseDateOfBirth = (dateOfBirth: unknown): string => {
-    if (!dateOfBirth || !isString(dateOfBirth) || !isDate(dateOfBirth)) {
-        throw new Error('Incorrect or missing date of birth: ' + dateOfBirth);
-    }
-    return dateOfBirth;
-};
-
-const parseSsn = (ssn: unknown): string => {
-    if (!ssn || !isString(ssn)) {
-        throw new Error('Incorrect or missing ssn');
-    }
-    return ssn;
-};
-
-const parseGender = (gender: unknown): Gender => {
-    if (!gender || !isString(gender) || !isGender(gender)) {
-        throw new Error('Incorrect or missing gender: ' + gender);
-    }
-    return gender;
-};
-
-const parseOccupation = (occupation: unknown): string => {
-    if (!occupation || !isString(occupation)) {
-        throw new Error('Incorrect or missing occupation');
-    }
-    return occupation;
-};
-
-const toNewPatient = (object: unknown): NewPatient => {
-    if (!object || typeof object !== 'object') {
-        throw new Error('Invalid input');
-    }
-
-    if ('name' in object && 'dateOfBirth' in object && 'ssn' in object
-        && 'gender' in object && 'occupation' in object) {
-
-        const newPatient: NewPatient = {
-            name: parseName(object.name),
-            dateOfBirth: parseDateOfBirth(object.dateOfBirth),
-            ssn: parseSsn(object.ssn),
-            gender: parseGender(object.gender),
-            occupation: parseOccupation(object.occupation)
-        };
-        return newPatient;
-    };
+export const NewPatientSchema = z.object({
+    name: z.string(),
+    dateOfBirth: z.iso.date(),
+    ssn: z.string(),
+    gender: z.enum(Gender),
+    occupation: z.string()
+});
 
 
-    throw new Error('Missing fields in input');
-};
-
-export default toNewPatient;
+export default "default";
